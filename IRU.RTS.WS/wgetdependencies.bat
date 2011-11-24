405cc845084d2d7fff72a491b3286e07637efdf0
David Schenk <David.Schenk@iru.org>
2011-11-24 17:42:16 +0100

    Updated Invalidated TIR Carnets Web Service
    * Move the wsdl and xsd files that are implemented from WIP folder to the Interface folder
    * WSDL has been amended to have qualified elements by default
    * xs:nonNegativeInteger type isn't handled correctly by C# (string) => changed to xs:unsignedInt
    * Add batch to get the dependencies: WSS assemblies and external global XSDs

--- IRU.RTS.WS/wgetdependencies.bat
+++ IRU.RTS.WS/wgetdependencies.bat
@@ -9,3 +9,12 @@ move wss-1.1.0.0-3.5.dll Dependencies\IRU.Common.WCF.Security.WSS.dll
 
 wget http://apps.charybdis.iruworld.org/swf/artifactory/libs-release-local/org/iru/common/wcf/wsdl/1.1.0.0/wsdl-1.1.0.0-3.5.dll
 move wsdl-1.1.0.0-3.5.dll Dependencies\IRU.Common.WCF.Wsdl.dll
+
+wget http://apps.charybdis.iruworld.org/swf/artifactory/libs-release-local/org/iru/common/iru-schemas/1.1/iru-schemas-1.1-iso-3166-1-alpha-3.xsd
+move iru-schemas-1.1-iso-3166-1-alpha-3.xsd CarnetService\IRU.RTS.WS.CarnetService.Interface\CarnetService-1\iso-3166-1-alpha-3.xsd
+
+wget http://apps.charybdis.iruworld.org/swf/artifactory/libs-release-local/org/iru/common/iru-schemas/1.1/iru-schemas-1.1-tir-actor.xsd
+move iru-schemas-1.1-tir-actor.xsd CarnetService\IRU.RTS.WS.CarnetService.Interface\CarnetService-1\tir-actor-1.xsd
+
+wget http://apps.charybdis.iruworld.org/swf/artifactory/libs-release-local/org/iru/common/iru-schemas/1.1/iru-schemas-1.1-tir-carnet.xsd
+move iru-schemas-1.1-tir-carnet.xsd CarnetService\IRU.RTS.WS.CarnetService.Interface\CarnetService-1\tir-carnet-1.xsd
