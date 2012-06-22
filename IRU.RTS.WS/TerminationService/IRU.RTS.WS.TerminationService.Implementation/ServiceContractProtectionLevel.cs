422d0d04fab93de79d86acdb80aeae3703bc93fc
Olivier Michoud <omi@abissa.ch>
2012-06-22 16:54:54 +0200

    [CAPP-152] added WCF WS-Security with X509 Certificate on TerminationService
    - need more rework on WSDL replacer for WS_Policy configuration...

---
+++ IRU.RTS.WS/TerminationService/IRU.RTS.WS.TerminationService.Implementation/ServiceContractProtectionLevel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
+
+namespace IRU.RTS.WS.TerminationService.Implementation
+{
+    public class ServiceContractProtectionLevel : Attribute, IContractBehavior
+    {
+        //private System.Net.Security.ProtectionLevel _ProtectionLevel = SuccessOrFailure.None;
+
+        public ServiceContractProtectionLevel()
+        {
+        }
+
+        //public ServiceContractProtectionLevel(System.Net.Security.ProtectionLevel protectionLevel)
+        //{
+        //    _ProtectionLevel = protectionLevel;
+        //}
+
+        #region IContractBehavior Members
+
+        public void  AddBindingParameters(ContractDescription contractDescription, ServiceEndpoint endpoint, System.ServiceModel.Channels.BindingParameterCollection bindingParameters)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void  ApplyClientBehavior(ContractDescription contractDescription, ServiceEndpoint endpoint, System.ServiceModel.Dispatcher.ClientRuntime clientRuntime)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void  ApplyDispatchBehavior(ContractDescription contractDescription, ServiceEndpoint endpoint, System.ServiceModel.Dispatcher.DispatchRuntime dispatchRuntime)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void  Validate(ContractDescription contractDescription, ServiceEndpoint endpoint)
+        {
+            //throw new NotImplementedException();
+            BindingElementCollection bindingParameters = endpoint.Binding.CreateBindingElements();
+            AsymmetricSecurityBindingElement sec = bindingParameters.Find<AsymmetricSecurityBindingElement>();
+            //AsymmetricSecurityBindingElement sec = endpoint.Binding.GetProperty<AsymmetricSecurityBindingElement>();
+
+            //System.Reflection.PropertyInfo s = endpoint.Binding.GetType().GetProperty("Security");
+
+            if (sec != null)
+            {
+                switch (sec.MessageProtectionOrder)
+                {
+                    case System.ServiceModel.Security.MessageProtectionOrder.SignBeforeEncrypt:
+                        contractDescription.ProtectionLevel = System.Net.Security.ProtectionLevel.Sign;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        #endregion
+        }
+}
--- IRU.RTS.WS/TerminationService/IRU.RTS.WS.TerminationService.Implementation/TerminationService.cs
+++ IRU.RTS.WS/TerminationService/IRU.RTS.WS.TerminationService.Implementation/TerminationService.cs
@@ -11,13 +11,19 @@ using IRU.RTS.WS.TerminationService.Interface;
 namespace IRU.RTS.WS.TerminationService.Implementation
 {
     [ServiceBehavior(AddressFilterMode = AddressFilterMode.Any, Namespace = "http://rts.iru.org/services/TerminationService-1")]
-    [WsdlReplacer("IRU.RTS.WS.TerminationService.Interface, Version=1.5.0.0, Culture=neutral, PublicKeyToken=null", "TerminationService_1.TerminationService-1.wsdl", false)]
+    //[WsdlReplacer("IRU.RTS.WS.TerminationService.Interface, Version=1.5.0.0, Culture=neutral, PublicKeyToken=null", "TerminationService_1.TerminationService-1.wsdl", false)]
     [XsdReplacer(new string[] { "IRU.RTS.WS.TerminationService.Interface, Version=1.5.0.0, Culture=neutral, PublicKeyToken=null" }, new string[] { "TerminationService_1.rts-termination-1.xsd", "TerminationService_1.rts-carnet-1.xsd", "TerminationService_1.tir-carnet-1.xsd", "TerminationService_1.tir-actor-1.xsd", "TerminationService_1.iso-3166-1-alpha-3.xsd" })]
+    [ServiceContractProtectionLevel]
     public class TerminationService : TerminationServiceSEI
     {
         public override transmitTIROperationTerminationsResponse transmitTIROperationTerminations(transmitTIROperationTerminationsRequest request)
         {
-            throw new NotImplementedException();
+            transmitTIROperationTerminationsResponse res = new transmitTIROperationTerminationsResponse();
+            
+            res.transmissionTime = DateTime.Now;
+            res.success = true;
+
+            return res;
         }
 
         public override getReconciliationRequestsResponse getReconciliationRequests(getReconciliationRequestsRequest request)
