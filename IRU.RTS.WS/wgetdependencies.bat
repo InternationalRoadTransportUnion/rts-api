a8f38a90eeb6cde0ea0cfc1241d9a5bf37f06e79
David Schenk <David.Schenk@iru.org>
2012-10-29 16:59:53 +0100

    RTS+ Web Service - ref to Common WCF updated & Version number amended
    * Use the latest Common WCF assemblies (ver. 1.5.2.0)
    * To not clash with Common WCF version, the version of RTS+ WS is now 1.5.2.1

--- IRU.RTS.WS/wgetdependencies.bat
+++ IRU.RTS.WS/wgetdependencies.bat
@@ -4,19 +4,19 @@ REM Download dependencies on artifactory and rename them accordingly
 
 mkdir Dependencies
 
-set WCF_SECURITY_WSS_VERSION=1.5.1.0
+set COMMON_WCF_VERSION=1.5.2.0
 set COMMON_ENTLIB_VERSION=1.0.1.0
 set RTSDOTNET_CLIENT_VERSION=1.1.0.0
 set COMMON_IRU_SCHEMAS_VERSION=1.3
 
-wget http://apps.charybdis.iruworld.org/swf/artifactory/libs-release-local/org/iru/common/wcf/security/wss/%WCF_SECURITY_WSS_VERSION%/wss-%WCF_SECURITY_WSS_VERSION%-3.5.dll
-move /y wss-%WCF_SECURITY_WSS_VERSION%-3.5.dll Dependencies\IRU.Common.WCF.Security.WSS.dll
+wget http://apps.charybdis.iruworld.org/swf/artifactory/libs-release-local/org/iru/common/wcf/security/wss/%COMMON_WCF_VERSION%/wss-%COMMON_WCF_VERSION%-3.5.dll
+move /y wss-%COMMON_WCF_VERSION%-3.5.dll Dependencies\IRU.Common.WCF.Security.WSS.dll
 
-wget http://apps.charybdis.iruworld.org/swf/artifactory/libs-release-local/org/iru/common/wcf/wsdl/%WCF_SECURITY_WSS_VERSION%/wsdl-%WCF_SECURITY_WSS_VERSION%-3.5.dll
-move /y wsdl-%WCF_SECURITY_WSS_VERSION%-3.5.dll Dependencies\IRU.Common.WCF.Wsdl.dll
+wget http://apps.charybdis.iruworld.org/swf/artifactory/libs-release-local/org/iru/common/wcf/wsdl/%COMMON_WCF_VERSION%/wsdl-%COMMON_WCF_VERSION%-3.5.dll
+move /y wsdl-%COMMON_WCF_VERSION%-3.5.dll Dependencies\IRU.Common.WCF.Wsdl.dll
 
-wget http://apps.charybdis.iruworld.org/swf/artifactory/libs-release-local/org/iru/common/wcf/behaviors/%WCF_SECURITY_WSS_VERSION%/behaviors-%WCF_SECURITY_WSS_VERSION%-3.5.dll
-move /y behaviors-%WCF_SECURITY_WSS_VERSION%-3.5.dll Dependencies\IRU.Common.WCF.Behaviors.dll
+wget http://apps.charybdis.iruworld.org/swf/artifactory/libs-release-local/org/iru/common/wcf/behaviors/%COMMON_WCF_VERSION%/behaviors-%COMMON_WCF_VERSION%-3.5.dll
+move /y behaviors-%COMMON_WCF_VERSION%-3.5.dll Dependencies\IRU.Common.WCF.Behaviors.dll
 
 wget http://apps.charybdis.iruworld.org/swf/artifactory/libs-release-local/org/iru/common/entlib/%COMMON_ENTLIB_VERSION%/entlib-%COMMON_ENTLIB_VERSION%-3.5.dll
 move /y entlib-%COMMON_ENTLIB_VERSION%-3.5.dll Dependencies\IRU.Common.EnterpriseLibrary.dll
