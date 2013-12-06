0133a8ed0fbc7a4ab3ba9a3e46d460769fe0fba2
David Schenk <David.Schenk@iru.org>
2013-12-06 10:59:24 +0100

    Updated RTS+ Web Services, Admin Client & WSTCHQ Crypto Engine
    * NUnit is now available on Bamboo => test projects are re-enabled in IRU.RTS.WS
    * Admin Client uses really the IRU Enterprise Library 1.0.3.0 => changed in config file
    * wgetdependencies.bat and wgetcipher.bat have been merged
    * Move the dependency to Common C# Cipher from 1.0.0.0 to 1.1.0.0
    
    * The Test projects are now temporary disabled in Release mode from
    * IRU.RTS.WS.sln,
      because NUnit is not yet installed on Bamboo Windows agents

--- wgetcipher.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM Download dependencies on artifactory and rename them accordingly
-
-mkdir Dependencies
-
-wget http://repo.iruworld.org/libs-release/org/iru/common/crypto/ciphersharp/1.0.0.0/ciphersharp-1.0.0.0-2.0.dll
-move ciphersharp-1.0.0.0-2.0.dll Dependencies\IRU.Common.Crypto.Cipher.dll
-
