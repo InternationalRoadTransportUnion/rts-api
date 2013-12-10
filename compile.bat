a95cc8b39ffeb187de221cdd8ca37446c60dc9fb
Nicolas Puzin <nicolas.puzin@iru.org>
2013-12-10 12:37:27 +0100

    compile.bat configuration

--- compile.bat
+++ compile.bat
@@ -6,8 +6,8 @@ mkdir RTS\WSST_Ex
 mkdir RTS\WSST_In
 msbuild WSTCHQ_BE/WSTCHQ_BE.sln /p:Configuration=Release;Platform="Any CPU" /t:rebuild
 xcopy /e /y "WSTCHQ_BE\TCHQHostService\bin\*.*" "RTS\TCHQ"
-xcopy /e /y "WSTCHQ_BE\FileListener\bin\*.*" "RTS\TCHQ"
 xcopy /e /y "WSTCHQ_BE\TCHQ_Processor\bin\*.*" "RTS\TCHQ"
+xcopy /e /y "WSTCHQ_BE\FileListener\bin\*.*" "RTS\TCHQ"
 xcopy /e /y "WSTCHQ_BE\SubscriberDBHelper\bin\*.*" "RTS\TCHQ"
 xcopy /e /y "WSTCHQ_BE\KeyManager\bin\*.*" "RTS\TCHQ"
 xcopy /e /y "WSTCHQ_BE\CryptEngine\bin\*.*" "RTS\TCHQ"
@@ -15,13 +15,17 @@ xcopy /e /y "WSTCHQ_BE\CryptoInterfaces\bin\*.*" "RTS\TCHQ"
 xcopy /e /y "WSTCHQ_BE\RTSStructures\bin\*.*" "RTS\TCHQ"
 msbuild WSST_BE/WSST_BE.sln /p:Configuration=Release;Platform="Any CPU" /t:rebuild
 xcopy /e /y "WSST_BE\WSST_ProcessorHostService\bin\*.*" "RTS\WSST_In"
+xcopy /e /y "WSST_BE\WSST_Processor\bin\*.*" "RTS\WSST_In"
 xcopy /e /y "WSST_BE\WSST_FileRcvrHostService\bin\*.*" "RTS\WSST_Ex"
+xcopy /e /y "WSST_BE\WSST_FileReceiver\bin\*.*" "RTS\WSST_Ex"
 msbuild WSRQ_BE/WSRQ_BE.sln /p:Configuration=Release;Platform="Any CPU" /t:rebuild
 xcopy /e /y "WSRQ_BE\WSRQHostService\bin\*.*" "RTS\WSRQ"
-xcopy /e /y "WSRQ_BE\WSRQNewRequest_ProcessorHostService\bin\*.*" "RTS\WSRQNR"
+xcopy /e /y "WSRQ_BE\WSRQ_QueryProcessor\bin\*.*" "RTS\WSRQ"
 msbuild WSRE_BE/WSRE_BE.sln /p:Configuration=Release;Platform="Any CPU" /t:rebuild
 xcopy /e /y "WSRE_BE\WSRE_ProcessorHostService\bin\*.*" "RTS\WSST_In"
+xcopy /e /y "WSRE_BE\WSRE_Processor\bin\*.*" "RTS\WSST_In"
 xcopy /e /y "WSRE_BE\WSRE_FileRcvrHostService\bin\*.*" "RTS\WSST_Ex"
+xcopy /e /y "WSRE_BE\WSRE_FileReceiver\bin\*.*" "RTS\WSST_Ex"
 del /f /s /q .\RTS\*.config
 del /f /s /q .\RTS\*.xml
 del /f /s /q .\RTS\*.vshost.*
--- uninstall-services.bat
+++ uninstall-services.bat
@@ -3,6 +3,5 @@ SET ENVIRONMENT=PREPROD
 "C:\Windows\Microsoft.NET\Framework\v2.0.50727\installutil" /ServiceName="WSST_Processor Host Service_%ENVIRONMENT%" /DisplayName="WSST Processor Host Service (%ENVIRONMENT%)" "WSST_In/WSST_ProcessorHostService.exe" /u
 "C:\Windows\Microsoft.NET\Framework\v2.0.50727\installutil" /ServiceName="WSST_File Rcvr Host Service_%ENVIRONMENT%" /DisplayName="WSST File Receiver (%ENVIRONMENT%)" "WSST_Ex/WSST_FileRcvrHostService.exe" /u
 "C:\Windows\Microsoft.NET\Framework\v2.0.50727\installutil" /ServiceName="WSRQHostService_%ENVIRONMENT%" /DisplayName="WSRQ Query Host Service (%ENVIRONMENT%)" "WSRQ/WSRQHostService.exe" /u
-"C:\Windows\Microsoft.NET\Framework\v2.0.50727\installutil" /ServiceName="WSRQ New Request Processor Host Service_%ENVIRONMENT%" /DisplayName="WSRQ New Request Processor Host Service (%ENVIRONMENT%)" "WSRQNR/WSRQNewRequest_ProcessorHostService.exe" /u
 "C:\Windows\Microsoft.NET\Framework\v2.0.50727\installutil" /ServiceName="WSRE_Processor Host Service_%ENVIRONMENT%" /DisplayName="WSRE Processor Host Service (%ENVIRONMENT%)" "WSST_In/WSRE_ProcessorHostService.exe" /u
 "C:\Windows\Microsoft.NET\Framework\v2.0.50727\installutil" /ServiceName="WSRE_File Rcvr Host Service_%ENVIRONMENT%" /DisplayName="WSRE File Receiver (%ENVIRONMENT%)" "WSST_Ex/WSRE_FileRcvrHostService.exe" /u
