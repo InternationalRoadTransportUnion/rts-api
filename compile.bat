8865e57db98bcfc47a0e6ba09543c46c173166b8
Nicolas Puzin <nicolas.puzin@iru.org>
2013-12-10 10:33:47 +0100

    dependencies for TCHQ

--- compile.bat
+++ compile.bat
@@ -6,6 +6,13 @@ mkdir RTS\WSST_Ex
 mkdir RTS\WSST_In
 msbuild WSTCHQ_BE/WSTCHQ_BE.sln /p:Configuration=Release;Platform="Any CPU" /t:rebuild
 xcopy /e /y "WSTCHQ_BE\TCHQHostService\bin\*.*" "RTS\TCHQ"
+xcopy /e /y "WSTCHQ_BE\FileListener\bin\*.*" "RTS\TCHQ"
+xcopy /e /y "WSTCHQ_BE\TCHQ_Processor\bin\*.*" "RTS\TCHQ"
+xcopy /e /y "WSTCHQ_BE\SubscriberDBHelper\bin\*.*" "RTS\TCHQ"
+xcopy /e /y "WSTCHQ_BE\KeyManager\bin\*.*" "RTS\TCHQ"
+xcopy /e /y "WSTCHQ_BE\CryptEngine\bin\*.*" "RTS\TCHQ"
+xcopy /e /y "WSTCHQ_BE\CryptoInterfaces\bin\*.*" "RTS\TCHQ"
+xcopy /e /y "WSTCHQ_BE\RTSStructures\bin\*.*" "RTS\TCHQ"
 msbuild WSST_BE/WSST_BE.sln /p:Configuration=Release;Platform="Any CPU" /t:rebuild
 xcopy /e /y "WSST_BE\WSST_ProcessorHostService\bin\*.*" "RTS\WSST_In"
 xcopy /e /y "WSST_BE\WSST_FileRcvrHostService\bin\*.*" "RTS\WSST_Ex"
