7288a72a008b38486a25678fe719c8e9f9ea155d
Nicolas Puzin <nicolas.puzin@iru.org>
2013-12-12 16:29:36 +0100

    compilation

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 rmdir /Q /S RTS
-SET CONFIG=Debug
+SET CONFIG=Release
 msbuild WSTCHQ_BE/TCHQHostService/TCHQHostService.csproj /p:Configuration=%CONFIG% /t:rebuild /p:OutputPath=./../../RTS/TCHQ/
 msbuild WSRQ_BE/WSRQHostService/WSRQHostService.csproj /p:Configuration=%CONFIG% /t:rebuild /p:OutputPath=./../../RTS/WSRQ/
 msbuild WSRQ_BE/WSRQNewRequest_ProcessorHostService/WSRQNewRequest_ProcessorHostService.csproj /p:Configuration=%CONFIG% /t:rebuild /p:OutputPath=./../../RTS/WSRQNR/
@@ -7,3 +7,5 @@ msbuild WSST_BE/WSST_ProcessorHostService/WSST_ProcessorHostService.csproj /p:Co
 msbuild WSST_BE/WSST_FileRcvrHostService/WSST_FileRcvrHostService.csproj /p:Configuration=%CONFIG% /t:rebuild /p:OutputPath=./../../RTS/WSST_Ex/
 msbuild WSRE_BE/WSRE_ProcessorHostService/WSRE_ProcessorHostService.csproj /p:Configuration=%CONFIG% /t:rebuild /p:OutputPath=./../../RTS/WSST_In/
 msbuild WSRE_BE/WSRE_FileRcvrHostService/WSRE_FileRcvrHostService.csproj /p:Configuration=%CONFIG% /t:rebuild /p:OutputPath=./../../RTS/WSST_Ex/
+rem del /f /s /q .\RTS\*.config
+rem del /f /s /q .\RTS\*.xml
