cb44ba2f26637f0864d92662879afc68087dc2df
Nicolas Puzin <nicolas.puzin@iru.org>
2013-12-16 11:19:21 +0100

    added the msbuild tasks to publish the web services

--- compile.bat
+++ compile.bat
@@ -7,5 +7,11 @@ msbuild WSST_BE/WSST_ProcessorHostService/WSST_ProcessorHostService.csproj /p:Co
 msbuild WSST_BE/WSST_FileRcvrHostService/WSST_FileRcvrHostService.csproj /p:Configuration=%CONFIG% /t:rebuild /p:OutputPath=./../../RTS/WSST_Ex/
 msbuild WSRE_BE/WSRE_ProcessorHostService/WSRE_ProcessorHostService.csproj /p:Configuration=%CONFIG% /t:rebuild /p:OutputPath=./../../RTS/WSST_In/
 msbuild WSRE_BE/WSRE_FileRcvrHostService/WSRE_FileRcvrHostService.csproj /p:Configuration=%CONFIG% /t:rebuild /p:OutputPath=./../../RTS/WSST_Ex/
+
+msbuild RTS_WS/TCHQ_WS/TCHQ_WS.sln /t:rebuild /p:Configuration=%CONFIG%;OutDir=.\..\..\RTS\TCHQ_WS\bin\;WebProjectOutputDir=.\..\..\..\RTS\TCHQ_WS\
+msbuild RTS_WS/WSRE_WS/WSRE_WS.sln /t:rebuild /p:Configuration=%CONFIG%;OutDir=.\..\..\RTS\WSRE_WS\bin\;WebProjectOutputDir=.\..\..\RTS\WSRE_WS\
+msbuild RTS_WS/WSRQ_WS/WSRQ_WS.sln /t:rebuild /p:Configuration=%CONFIG%;OutDir=.\..\..\RTS\WSRQ_WS\bin\;WebProjectOutputDir=.\..\..\RTS\WSRQ_WS\
+msbuild RTS_WS/WSST_WS/WSST_WS.sln /t:rebuild /p:Configuration=%CONFIG%;OutDir=.\..\..\RTS\WSST_WS\bin\;WebProjectOutputDir=.\..\..\..\RTS\WSST_WS\
+
 rem del /f /s /q .\RTS\*.config
 rem del /f /s /q .\RTS\*.xml
