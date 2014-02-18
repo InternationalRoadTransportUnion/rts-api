86b6b5a14df094e53ddb6ab18055406c3b6016ce
David Schenk <David.Schenk@iru.org>
2014-02-18 16:02:48 +0100

    Upgrade dependencies in RTS+ and AdminClient
    * IRU.CommonEnterpriseLibrary from v1.0.3.0 to v1.1.0.0
      - Move the commandTimeout parameter from IRUSqlCommand section to connectionString
    * IRU.Common.WCF from v1.6.7.0 to v1.6.9.0
      - Fix a bug in Endpoint URI translation with SOAP 1.1 services
    * RTSDotNETClient from v1.2.2 => v1.6.0
      - Use the latest build after changes done by MVE

--- wgetdependencies.bat
+++ wgetdependencies.bat
@@ -7,9 +7,9 @@ mkdir IRU.RTS.WS\Dependencies
 
 set REPO_LIBS_RELEASE_LOCAL_URL=http://repo.iruworld.org/libs-release-local
 set COMMON_CRYPTO_VERSION=1.1.0.0
-set COMMON_WCF_VERSION=1.6.7.0
-set COMMON_ENTLIB_VERSION=1.0.3.0
-set RTSDOTNET_CLIENT_VERSION=1.2.2
+set COMMON_WCF_VERSION=1.6.9.0
+set COMMON_ENTLIB_VERSION=1.1.0.0
+set RTSDOTNET_CLIENT_VERSION=1.6.0
 set COMMON_IRU_SCHEMAS_VERSION=1.5
 
 wget %REPO_LIBS_RELEASE_LOCAL_URL%/org/iru/common/crypto/ciphersharp/%COMMON_CRYPTO_VERSION%/ciphersharp-%COMMON_CRYPTO_VERSION%-3.5.dll
@@ -27,7 +27,7 @@ move /y behaviors-%COMMON_WCF_VERSION%-3.5.dll IRU.RTS.WS\Dependencies\IRU.Commo
 wget %REPO_LIBS_RELEASE_LOCAL_URL%/org/iru/common/entlib/%COMMON_ENTLIB_VERSION%/entlib-%COMMON_ENTLIB_VERSION%-3.5.dll
 move /y entlib-%COMMON_ENTLIB_VERSION%-3.5.dll IRU.RTS.WS\Dependencies\IRU.Common.EnterpriseLibrary.dll
 
-wget %REPO_LIBS_RELEASE_LOCAL_URL%/org/iru/rts/rtsdotnet-client/%RTSDOTNET_CLIENT_VERSION%/rtsdotnet-client-%RTSDOTNET_CLIENT_VERSION%-3.5.dll
+wget %REPO_LIBS_RELEASE_LOCAL_URL%/org/iru/rts/classic/rtsdotnet-client/%RTSDOTNET_CLIENT_VERSION%/rtsdotnet-client-%RTSDOTNET_CLIENT_VERSION%-3.5.dll
 move /y rtsdotnet-client-%RTSDOTNET_CLIENT_VERSION%-3.5.dll IRU.RTS.WS\Dependencies\RTSDotNETClient.dll
 
 wget %REPO_LIBS_RELEASE_LOCAL_URL%/org/iru/common/iru-schemas/%COMMON_IRU_SCHEMAS_VERSION%/iru-schemas-%COMMON_IRU_SCHEMAS_VERSION%-iso-3166-1-alpha-3.xsd
