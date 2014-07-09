c49be4d7cb5a071638ecc1f36a595c981d58dcd7
David Schenk <David.Schenk@iru.org>
2014-07-09 19:48:09 +0200

    Re-enable the TIREPDB2G task in RTS build plan in Bamboo

--- classic/compile.bat
+++ classic/compile.bat
@@ -8,12 +8,15 @@ msbuild WSST_BE/WSST_FileRcvrHostService/WSST_FileRcvrHostService.csproj /p:Conf
 msbuild WSRE_BE/WSRE_ProcessorHostService/WSRE_ProcessorHostService.csproj /p:Configuration=%CONFIG% /t:rebuild /p:OutputPath=./../../RTS/WSST_In/
 msbuild WSRE_BE/WSRE_FileRcvrHostService/WSRE_FileRcvrHostService.csproj /p:Configuration=%CONFIG% /t:rebuild /p:OutputPath=./../../RTS/WSST_Ex/
 msbuild WSTVQR_BE/TVQR_Processor/TVQR_Processor.csproj /p:Configuration=%CONFIG% /t:rebuild /p:OutputPath=./../../RTS/TVQR/
+msbuild TIREPD_RTS/TIREDPG2BRecvrHostService/TIREDPG2BRecvrHostService.csproj /p:Configuration=%CONFIG% /t:rebuild /p:OutputPath=./../../RTS/TIREPDG2B_SVC/
 
 msbuild RTS_WS/TCHQ_WS/TCHQ_WS.sln /t:rebuild /p:Configuration=%CONFIG%;OutDir=.\..\..\RTS\TCHQ_WS\bin\;WebProjectOutputDir=.\..\..\..\RTS\TCHQ_WS\
 msbuild RTS_WS/WSRE_WS/WSRE_WS.sln /t:rebuild /p:Configuration=%CONFIG%;OutDir=.\..\..\RTS\WSRE_WS\bin\;WebProjectOutputDir=.\..\..\RTS\WSRE_WS\
 msbuild RTS_WS/WSRQ_WS/WSRQ_WS.sln /t:rebuild /p:Configuration=%CONFIG%;OutDir=.\..\..\RTS\WSRQ_WS\bin\;WebProjectOutputDir=.\..\..\RTS\WSRQ_WS\
 msbuild RTS_WS/WSST_WS/WSST_WS.sln /t:rebuild /p:Configuration=%CONFIG%;OutDir=.\..\..\RTS\WSST_WS\bin\;WebProjectOutputDir=.\..\..\..\RTS\WSST_WS\
 msbuild RTS_WS/TVQR_WS/TVQR_WS.sln /t:rebuild /p:Configuration=%CONFIG%;OutDir=.\..\..\RTS\TVQR_WS\bin\;WebProjectOutputDir=.\..\..\..\RTS\TVQR_WS\
+msbuild TIREPDB2G/TIREPDB2G/TIREPDB2G.csproj /t:rebuild /p:Configuration=%CONFIG%;OutDir=.\..\..\RTS\TIREPDB2G\bin\;WebProjectOutputDir=.\..\..\RTS\TIREPDB2G\
+msbuild TIREPDB2G/TIREPDG2B/TIREPDG2B.csproj /t:rebuild /p:Configuration=%CONFIG%;OutDir=.\..\..\RTS\TIREPDG2B\bin\;WebProjectOutputDir=.\..\..\RTS\TIREPDG2B\
 
 rem del /f /s /q .\RTS\*.config
 rem del /f /s /q .\RTS\*.xml
