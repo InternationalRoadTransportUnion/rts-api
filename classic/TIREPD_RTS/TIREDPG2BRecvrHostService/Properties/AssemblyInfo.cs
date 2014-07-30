3f9f1f80dc5d596af16a848884d62c2df1b53ad3
David Schenk <David.Schenk@iru.org>
2014-07-30 19:02:47 +0200

    Release 2.0.0 of RTS classic

--- classic/TIREPD_RTS/TIREDPG2BRecvrHostService/Properties/AssemblyInfo.cs
+++ classic/TIREPD_RTS/TIREDPG2BRecvrHostService/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.0.14195")]
-[assembly: AssemblyFileVersion("2.0.0.14195")]
+[assembly: AssemblyVersion("2.0.0.65534")]
+[assembly: AssemblyFileVersion("2.0.0.65534")]
