eb98be36c0cbde12b9c7834c074aeb5f6f58c924
David Schenk <David.Schenk@iru.org>
2014-08-04 16:39:26 +0200

    Get ready for next development phase of RTS classic (2.0.2 [dev])

--- classic/TIREPD_RTS/TestSender/Properties/AssemblyInfo.cs
+++ classic/TIREPD_RTS/TestSender/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.1.65534")]
-[assembly: AssemblyFileVersion("2.0.1.65534")]
+[assembly: AssemblyVersion("2.0.2.14216")]
+[assembly: AssemblyFileVersion("2.0.2.14216")]
