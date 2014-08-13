29885b25f71e8172e906938ae3da58f38bd72856
David Schenk <David.Schenk@iru.org>
2014-08-13 10:35:05 +0200

    Release 2.0.4 of RTS classic

--- classic/TIREPD_RTS/TIREPDB2GSendHostService/Properties/AssemblyInfo.cs
+++ classic/TIREPD_RTS/TIREPDB2GSendHostService/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.4.14219")]
-[assembly: AssemblyFileVersion("2.0.4.14219")]
+[assembly: AssemblyVersion("2.0.4.65534")]
+[assembly: AssemblyFileVersion("2.0.4.65534")]
