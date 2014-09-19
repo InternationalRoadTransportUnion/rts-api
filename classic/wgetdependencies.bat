35cf3c08b324df3b8a72df52d53de7e33dd6d621
David Schenk <David.Schenk@iru.org>
2014-09-19 18:02:29 +0200

    The TIREPD RTS Services use now the EPD messages schemas published in Artifactory

--- classic/wgetdependencies.bat
+++ classic/wgetdependencies.bat
@@ -9,3 +9,9 @@ set COMMON_CRYPTO_VERSION=1.1.0.0
 
 wget %REPO_LIBS_RELEASE_LOCAL_URL%/org/iru/common/crypto/ciphersharp/%COMMON_CRYPTO_VERSION%/ciphersharp-%COMMON_CRYPTO_VERSION%-3.5.dll
 move ciphersharp-%COMMON_CRYPTO_VERSION%-3.5.dll Dependencies\IRU.Common.Crypto.Cipher.dll
+
+REM Call other scripts
+
+cd TIREPD_RTS
+call wgetdepschemas.bat
+cd ..
