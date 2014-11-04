55740f5a07a70e1b051c77ffba8e2f3cbbce97a5
David Schenk <David.Schenk@iru.org>
2014-11-04 10:33:41 +0100

    Updated epd-messages dependencies in G2B RTS backend service
    * The schemas of epd-messages is now v1.0.5

--- classic/TIREPD_RTS/wgetdepschemas.bat
+++ classic/TIREPD_RTS/wgetdepschemas.bat
@@ -3,7 +3,7 @@
 REM Download EPD messages schemas on artifactory and rename them accordingly
 
 set REPO_LIBS_RELEASE_LOCAL_URL=http://repo.iruworld.org/libs-release-local
-set EPD_MESSAGES_VERSION=1.0.1
+set EPD_MESSAGES_VERSION=1.0.5
 set WGET_PARAMS=--no-proxy
 
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
@@ -16,3 +16,7 @@ for %%s in (004 005 009 013 014 015 016 025 028 029 045 051 055 060 917 928) do 
 set RFILE=epd-messages-%EPD_MESSAGES_VERSION%-types.xsd
 wget %WGET_PARAMS% %REPO_LIBS_RELEASE_LOCAL_URL%/org/iru/epd/epd-messages/%EPD_MESSAGES_VERSION%/!RFILE!
 move !RFILE! TIREDPG2BRecvrHostService\Schemas\TIREPD_Types.xsd
+
+set RFILE=epd-messages-%EPD_MESSAGES_VERSION%-set.xsd
+wget %WGET_PARAMS% %REPO_LIBS_RELEASE_LOCAL_URL%/org/iru/epd/epd-messages/%EPD_MESSAGES_VERSION%/!RFILE!
+move !RFILE! TIREDPG2BRecvrHostService\Schemas\TIREPD_Set.xsd
